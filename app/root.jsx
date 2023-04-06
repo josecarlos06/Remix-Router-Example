0e6dcc88b07e088f155f568bb3db73867cbe1d78
josecarlos06 <carlosmedina.vasq06@gmail.com>
2023-04-05 21:44:33 -0600

    metadata

--- app/root.jsx
+++ app/root.jsx
@@ -3,22 +3,37 @@ import {
   LiveReload,
   Meta,
   Outlet,
-  Scripts,
-  ScrollRestoration,
+  Scripts
 } from "@remix-run/react";
 
-export default function App() {
+// informacion meta
+export function meta(){
+   return [
+      {charSet : 'utf-8'},
+      { 
+         name : "viewport", 
+         content : "width=device-width, initial-scale=1.0"
+      },
+      {title : "Example meta V2"}
+   ]
+}
+
+export default function App(){
+   return(
+      <Document>
+         <Outlet/>
+      </Document>
+   )
+}
+function Document({children}) {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
+         {children}
         <Scripts />
         <LiveReload />
       </body>
